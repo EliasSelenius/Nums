3ba935699c0a50705994d366016d4cfb3489a2ea
Elias Sebastian Selenius Haneborg <elias.sebastian.selenius@gmail.com>
2018-10-18 22:45:32 +0200

    abstract class Shape is now interface IShape

---
+++ Geometry/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums.Geometry {
+    public interface IShape {
+        float Volume { get; set; }
+        float Area { get; set; }
+        float FurthestLengthFromPivot { get; set; }
+    }
+}
