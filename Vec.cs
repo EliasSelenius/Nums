b4aa4dfad79587bd2e1b81474033a5edd47371a9
Elias Sebastian Haneborg <elias@nextron.no>
2018-11-09 09:38:24 +0100

    Start of some refactoring

---
+++ Vec.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums {
+
+    public interface IArithmetic<T, U> {
+        T Add(U a);
+        T Sub(U a);
+        T Mul(U a);
+        T Div(U a);
+    }
+
+    public abstract class Vec<T> where T : Vec<T>, IArithmetic<T, T>, IArithmetic<T, float> {
+        public static Vec<T> operator *(T a, float b) => a.Add(b);
+        public static Vec3 operator /(Vec3 a, float b) => new Vec3(a.x / b, a.y / b, a.z / b);
+        public static Vec3 operator *(Vec3 a, Vec3 b) => new Vec3(a.x * b.x, a.y * b.y, a.z * b.z);
+        public static Vec3 operator /(Vec3 a, Vec3 b) => new Vec3(a.x / b.x, a.y / b.y, a.z / b.z);
+        public static Vec3 operator +(Vec3 a, Vec3 b) => new Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+        public static Vec3 operator -(Vec3 a, Vec3 b) => new Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+        public static Vec3 operator -(Vec3 a) => new Vec3(-a.x, -a.y, -a.z);
+
+        
+    }
+}
